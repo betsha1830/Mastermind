976afad8a401a5fc8f94a3fe071a6f87e5ccf478
Betsue Weldemariam <btsegay0@gmail.com>
2022-04-04 23:01:37 +0300

    pos and color check functions done.

--- game_logic.rb
+++ game_logic.rb
@@ -1,8 +1,24 @@
 require_relative 'mastermind.rb'
 
 class GameLogic
+  
+  def pos_check
+    counter = 0
+    while counter < 4
+      correct_place += 1 if (creator_color[counter] == finder_color[counter])
+      counter += 1
+    end
+  end
 
-
-
+  def color_check
+    for (i = 0; i < 4; i++)
+      for (j = 0; j < 4; j++)
+        if (finder_color[j] == creator_color[i])
+          correct_color += 1
+          finder_color[j] = ""
+        end
+      end
+    end
+  end
 
 end
