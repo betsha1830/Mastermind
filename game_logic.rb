4be16cc0761639e7860fad875d5d34651415eb9f
Betsue Weldemariam <btsegay0@gmail.com>
2022-04-05 16:00:53 +0300

    Module not working.

--- game_logic.rb
+++ game_logic.rb
@@ -1,24 +1,31 @@
 require_relative 'mastermind.rb'
 
 class GameLogic
+
+  include Shared
   
   def pos_check
     counter = 0
     while counter < 4
-      correct_place += 1 if (creator_color[counter] == finder_color[counter])
+      @correct_place += 1 if (@creator_color[counter] == @finder_color[counter])
       counter += 1
     end
   end
 
   def color_check
-    for (i = 0; i < 4; i++)
-      for (j = 0; j < 4; j++)
-        if (finder_color[j] == creator_color[i])
+    temp = creator_color
+    finder_color.each_char do |f_color|
+      temp.each_char do |c_color|
+        if (f_color == c_color)
           correct_color += 1
-          finder_color[j] = ""
+          f_color = " "
+          c_color = " "
         end
       end
     end
   end
 
 end
+
+x = GameLogic.new()
+puts x.pos_check
