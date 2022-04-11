db7a8f3d7db890979f1e676254c5a7b5347e763e
Betsue Weldemariam <btsegay0@gmail.com>
2022-04-11 11:49:20 +0300

    guess_input_prompt function takes a proper input from user.

---
+++ prompt.rb
@@ -0,0 +1,57 @@
+require_relative "game_logic.rb"
+
+class Prompt < GameLogic
+
+  def against_prompt
+    input = gets.chomp.to_i
+    until input == 1 || input == 2
+      puts "Please enter a correct input.\n1. Computer\n2. Friend (hard to find these days :)"
+    end
+    if input == 1
+      @against = 1
+    else 
+      @against = 2
+    end
+  end
+
+  def type_prompt
+    input = gets.chomp.to_i
+    until input == 1 || input == 2
+      puts "Please enter a correct input.\n1. Finder\n2. Creator"
+    end
+    if input == 1
+      @type = 1
+    else 
+      @type = 2
+    end
+  end
+
+  def guess_input_prompt
+    input = gets.chomp
+    until input.length == 4
+      puts "Please enter a guess that contains 4 colors."
+      input = gets.chomp
+    end
+    i = 0
+    while i < 4
+      if !(input[i].to_i <= 6 && input[i].to_i >= 1)
+        puts "Please input a number that is between 1 and 6."
+        input = gets.chomp
+      else i += 1
+      end
+    end
+    @finder_color = input
+  end
+
+
+  def repeat_color_prompt
+    input = gets.chomp
+    until input == "y" || input == "n"
+      puts "Please enter an appropriate response."
+    end
+    if input == "y" 
+
+    end
+  end
+
+end
