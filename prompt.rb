b38f235064dab9c1ee1e92aaf3e98f38e5f2b84a
Betsue Weldemariam <btsegay0@gmail.com>
2022-04-13 16:03:38 +0300

    player finder prompt moved to player class.

--- prompt.rb
+++ prompt.rb
@@ -6,6 +6,8 @@ class Prompt < GameLogic
     input = gets.chomp.to_i
     until input == 1 || input == 2
       puts "Please enter a correct input.\n1. Computer\n2. Friend (hard to find these days :)"
+      input = gets.chomp
+      input.to_i
     end
     if input == 1
       @against = 1
@@ -28,24 +30,6 @@ class Prompt < GameLogic
     end
   end
 
-  def guess_input_prompt
-    input = gets.chomp
-    until input.length == 4
-      puts "Please enter a guess that contains 4 colors."
-      input = gets.chomp
-    end
-    i = 0
-    while i < 4
-      if !(input[i].to_i <= 6 && input[i].to_i >= 1)
-        puts "Please input a number that is between 1 and 6."
-        input = gets.chomp
-      else i += 1
-      end
-    end
-    @finder_color = input
-  end
-
-
   def repeat_color_prompt
     input = gets.chomp
     until input == "y" || input == "n"
@@ -57,5 +41,4 @@ class Prompt < GameLogic
     else @no_repeat = true
     end
   end
-
 end
