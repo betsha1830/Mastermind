928ede314fc857585e8655c6469c62d5519afea8
Betsue Weldemariam <btsegay0@gmail.com>
2022-04-18 23:29:45 +0300

    Called the functions in an appropriate order. Needs more OOP refractoring.

--- game.rb
+++ game.rb
@@ -1,9 +1,20 @@
-require_relative 'mastermind.rb'
+require_relative 'game_logic'
+require_relative 'prompt'
+require_relative 'message'
+require_relative 'player'
 
-class GameBoard
+class Game
 
-  def draw
-
+  def game
+    rules
+    against
+    against_prompt
+    player_type
+    type_prompt
+    color_type
+    repeat_color_prompt
+    creator_color
+    
   end
 
 end
