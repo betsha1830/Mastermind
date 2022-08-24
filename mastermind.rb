31b88a26e21347cd3225b7ba2cecec4b6e0852fd
Betsue Weldemariam <btsegay0@gmail.com>
2022-08-24 20:39:53 +0300

    random color generator method added

--- mastermind.rb
+++ mastermind.rb
@@ -1,3 +1,19 @@
-module Shared
-  
+class MasterMind
+
+    def initialize
+
+    end
+
+    def random_color_generator
+        random_color = ""
+        random_num = Random.new()
+        until random_color.size == 4
+            random_color += (random_num.rand(6) + 1).to_s
+        end
+        random_color
+    end
+
 end
+
+mind = MasterMind.new
+puts mind.random_color_generator
